9a6ee41d73415897148f8f29d70678eb8679a766
voltagevalkyrie <voltagevalkyrie@gmail.com>
2024-03-13 17:55:37 +0530

    initial commit

---
+++ src/app/example/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { HeroParallaxDemo } from './HeroParallaxDemo';
+// import { ParallaxScrollDemo } from './ParallaxScrollDemo';
+// import { SparklesPreview } from './SparklesPreview';
+// import { TextRevealCardPreview } from './TextRevealCardPreview';
+// import { WavyBackgroundDemo } from './WavyBackgroundDemo';
+
+export default function Page() {
+  const [isTouchDevice, setIsTouchDevice] = useState(false);
+  
+  useEffect(() => {
+        setIsTouchDevice('ontouchstart' in window || navigator.maxTouchPoints > 0)
+  }, []);
+
+  return (
+    <>
+      <HeroParallaxDemo />
+      
+      {/* <ParallaxScrollDemo /> */}
+    </>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { GlowingStarsBackgroundCardPreview } from "./GlowingStarsBackgroundCardPreview";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <GlowingStarsBackgroundCardPreview />
+    </main>
+  );
+}
